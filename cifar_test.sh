4bacd47b3e7e60cd48bdc07b5c8eb64ca900bdb5
Youzhi (Will) Zou <wzou@fire2.nec-labs.com>
2013-08-07 21:17:38 -0400

    adding cifar_test

---
+++ cifar_test.sh
@@ -0,0 +1,1 @@
+python convnet.py --data-path=/mnt/exthdd/cifar-10-py-colmajor/ --save-path=./tmp --test-range=5 --train-range=1-4 --layer-def=./example-layers/layers-conv-local-13pct.cfg --layer-params=./example-layers/layer-params-conv-local-13pct.cfg --data-provider=cifar-cropped --test-freq=13 --crop-border=4 --epochs=100 
